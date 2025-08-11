f2985c02ebbb511ef47c690955cfc6cff3f388f6
prarabdh14 <prarabdhatrey@gmail.com>
2025-08-11 21:44:43 +0530

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,6 +13,7 @@ import TripBudget from './components/TripBudget'
 import TripCalendar from './components/TripCalendar'
 import UserProfile from './components/UserProfile'
 import AdminDashboard from './components/AdminDashboard'
+import AiItineraryGenerator from './components/AiItineraryGenerator'
 import Header from './components/Header'
 
 import VantaGlobe from './components/VantaGlobe'
@@ -81,6 +82,7 @@ function App() {
             <Route path="/trip/:id/calendar" element={<TripCalendar />} />
             <Route path="/search/cities" element={<CitySearch />} />
             <Route path="/search/activities" element={<ActivitySearch />} />
+            <Route path="/ai-itinerary" element={<AiItineraryGenerator />} />
             <Route path="/profile" element={<UserProfile user={currentUser} />} />
             <Route path="/admin" element={<AdminDashboard />} />
             <Route path="*" element={<Navigate to="/dashboard" replace />} />
